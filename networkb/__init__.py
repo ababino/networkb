1cb204d2cb33be5e624b642bce641ca29960a483
Andres Babino <ababino@gmail.com>
2013-07-23 20:12:11 -0300

    version in __init__.py

--- networkb/__init__.py
+++ networkb/__init__.py
@@ -6,3 +6,7 @@ from networkb.algorithms import *
 
 import networkb.ploters
 from networkb.ploters import *
+
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution("networkb").version
