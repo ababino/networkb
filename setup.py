e5bd714c018950679a818cecae91c6dcb6d6131a
Andres Babino <ababino@gmail.com>
2013-07-23 20:37:19 -0300

    Preparing release 0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='networkb',
-      version='0.1dev0',
+      version='0.1',
       description='Network analysis for brain data',
       author='Andres Babino',
       author_email='ababino@gmail.com',
