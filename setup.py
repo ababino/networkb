895ce8f0184e232fb074fe93db440dcde9a6e295
Andres Babino <ababino@gmail.com>
2013-07-24 12:08:25 -0300

    change version to 0.1.0 (add .0)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='networkb',
-      version='0.1',
+      version='0.1.0',
       description='Network analysis for brain data',
       author='Andres Babino',
       author_email='ababino@gmail.com',
