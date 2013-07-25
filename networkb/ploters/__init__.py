1c1d5bbdb1fd947ea7dac071057b78a15978be56
Andres Babino <ababino@gmail.com>
2013-07-25 19:00:19 -0300

    add plot_4d_brain

--- networkb/ploters/__init__.py
+++ networkb/ploters/__init__.py
@@ -3,3 +3,4 @@ from networkb.ploters.plot_percolation_curve import plot_percolation_curve
 from networkb.ploters.plot_NON import plot_NON
 from networkb.ploters.brain_plot import brain_plot
 from networkb.ploters.plot_clusters_in_brain import plot_clusters_in_brain
+from networkb.ploters.plot_4d_brain import plot_4d_brain
